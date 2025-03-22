7429616e6a5681a9af22b60a354e45e46209e05e
Nelson <nelson.atiang@student.moringaschool.com>
2025-03-22 17:45:47 +0300

    complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    
+  ]
+  function superbowlWin(record) {
+    const win = record.find(function(game)  {return game.result === "W"});
+    return   win ? win.year : undefined;
+}
+
+
+
+
+
+
